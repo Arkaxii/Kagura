8f754f3cd331ffa1186927f926c6b30b275229b4
Arkaxii <45296939+Arkaxii@users.noreply.github.com>
2018-12-23 12:41:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -763,9 +763,9 @@ if (randnum == 3){
 if (message.content === '<<avatar') {
     let Avatar_embed = new Discord.RichEmbed()
   .setColor('#275BF0')
-  .setTitle('${user.username}')
+  .setTitle('${author.username}')
   .setImage(message.author.avatarURL)
-  .setFooter('${user.username}')
+  .setFooter('${author.username}')
 
     message.channel.send(Avatar_embed)
 }
