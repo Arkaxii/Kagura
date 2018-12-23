7112f2be21ff5c9e1d36a51233578231e2c045c7
Arkaxii <45296939+Arkaxii@users.noreply.github.com>
2018-12-23 17:44:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -782,7 +782,7 @@ message.channel.send(embed)
 } */
  
  if(message.content.startsWith(prefix + "test")){
-    message.channel.send( message.author.id + ` TEEST`);
+    message.channel.send( message.author + ` TEEST`);
              }
  
 }); 
