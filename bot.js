5df17a7dad139d88a4c99f94e7650bf6f8ab3eb7
Arkaxii <45296939+Arkaxii@users.noreply.github.com>
2018-12-28 15:29:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -234,7 +234,8 @@ client.on("message", async message => {
             if(!member)
               return message.reply("Veuiller mentionner un utilisateur valide");
             money.updateBal(member.id, 500 ).then((i) => {  
-                message.channel.send(`**Tu as offer $500! à ${member.user.tag} **\n**Vous possédez actuellement:** ${i.money}`);
+                message.channel.send(`Tu as offer $500! à **${member.user.tag} **\n
+                                      Et à actuellement: **${i.money}***`);
             })
         }
         /*
