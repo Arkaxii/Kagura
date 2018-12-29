6a98908a87a3534bbe3d7b21f0a2af0da20a3a7e
Arkaxii <45296939+Arkaxii@users.noreply.github.com>
2018-12-30 00:20:07 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -133,7 +133,7 @@ client.on("message", async message => {
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 	
- /* if(command === "rainbow") {
+  if(command === "rainbow") {
     if(!message.member.permissions.has('ADMINISTRATOR') )
     return message.reply("Cette commande est réserver aux Admin");
     let rolerain = message.mentions.roles.first();
@@ -143,7 +143,7 @@ if(!rolerain)
        rolerain.setColor('RANDOM')
  }, 1000);
 	     message.channel.send(`le role ${rolerain} a été definit comme rainbow`)
-    }*/
+    }
     if(command === "ping") {
     const m = await message.channel.send("Ping?");
     m.edit(`Pong! Latence de ${m.createdTimestamp - message.createdTimestamp}ms. API Latence de ${Math.round(client.ping)}ms`);
