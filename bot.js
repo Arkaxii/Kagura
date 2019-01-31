4e6a3d2f747157ec5444f6cf076c54d225632b19
Arkaxii <45296939+Arkaxii@users.noreply.github.com>
2019-01-31 12:41:33 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -167,7 +167,10 @@ userAnswer = "";
 });
 
 client.on("message", async message => {
-  
+const config = require("./config.json");
+
+	    if (message.content.indexOf(config.prefix) !== 0) return;
+
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 	
