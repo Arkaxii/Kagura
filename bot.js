dca1b745d7a07f3f3f28114238416d8ed13dafd6
Arkaxii <45296939+Arkaxii@users.noreply.github.com>
2019-03-23 00:05:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -173,17 +173,17 @@ client.on('guildMemberRemove', member => {
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`Dans ${client.guilds.size} servers`);
 });
 
 client.on("guildCreate", guild => {
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`Dans ${client.guilds.size} servers`);
 });
 
 client.on("guildDelete", guild => {
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`Dans ${client.guilds.size} servers`);
 });
 client.on ("ready", () => {
 answered = true;
