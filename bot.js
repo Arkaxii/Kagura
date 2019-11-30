9240bd859d2c37df5d863f901694716e493b452c
Arkaxii <lucas51120@hotmail.com>
2019-11-30 22:58:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -467,17 +467,17 @@ client.on('guildMemberRemove', member => {
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  client.user.setActivity(`Regarder ${client.guilds.size} servers`);
+  client.user.setActivity(`squatter ${client.guilds.size} servers`);
 });
 
 client.on("guildCreate", guild => {
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`Regarder ${client.guilds.size} servers`);
+  client.user.setActivity(`squatter ${client.guilds.size} servers`);
 });
 
 client.on("guildDelete", guild => {
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Regarder ${client.guilds.size} servers`);
+  client.user.setActivity(`squatter ${client.guilds.size} servers`);
 });
 client.on ("ready", () => {
 answered = true;
